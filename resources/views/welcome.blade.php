3e9cec9b6a377f4ca9f9be388fe49e59d7c04954
Zhang Weipeng <719899730@qq.com>
2017-06-27 23:29:31 +0800

    test01

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    XINGXING
                 </div>
 
                 <div class="links">
