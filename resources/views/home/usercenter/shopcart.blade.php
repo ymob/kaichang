acb69162b20d5dc29ed0ce4320a908f549d4d6f2
Zhang Weipeng <719899730@qq.com>
2017-07-25 23:47:02 +0800

    zwp 个人中心购物车订单

--- resources/views/home/usercenter/shopcart.blade.php
+++ resources/views/home/usercenter/shopcart.blade.php
@@ -1,149 +1,88 @@
 @extends('home.usercenter.layout')
 @section('header')
+{{--    <link rel="stylesheet" href="{{ asset('/home/css/index/index2.css') }}">--}}
     <style>
-        .order-title {
-            border:1px solid #e3eaee;
-            /*padding:5px 0;*/
-            font-size:14px;
-            color:#001f3b;
-            height:40px;
-            width:715px;
-        }
-        a{
-            text-decoration:none;
-            color:#333;
-        }
-        .order-title li{
-            cursor:pointer;
-            float:left;
-            padding:5px 34px;
-            border-right:1px solid  #e3eaee;
-            position:relative;
-            height:40px;
-        }
-       a:hover{
-            color:#007bb6;
-            font-size:102%;
-        }
-         .style-table
-        {
-             border:1px solid #e3eaee;
-             width:715px;
-        }
-
-        .style-td{
-            font-size:16px;
-            font-family:serif,Times;
-            color:#001f3b;
-            border:1px solid  #e3eaee;
-            text-align:center;
-
-        }
-        td{
-            height:20px;
-        }
-        .current
-        {
-          border-bottom: 3px solid #1295bf;
-        }
+        .shopcartCon div{padding:5px 10px;}
+        .shopcartCon .wd{width:1000px;}
+        .shopcartCon #title1{font-weight:bold;}
+        .shopcartCon .shopcartDel:hover{color:red;}
+        .shopcartCon .bot{border-bottom:1px dashed black;}
+        .shopcartCon .botsolid{border-bottom:1px solid black;}
     </style>
 @endsection
 @section('con')
+    <div class="shopcartCon" >
 
-    <ul class="order-title">
-      <li ><a href="#" class="sta">全部订单</a></li>
-      <li><a href="#" class="sta">未付款</a></li>
-      <li ><a href="#" class="sta">未接单</a></li>
-      <li><a href="#" class="sta">使用中</a></li>
-      <li ><a href="#" class="sta">定单取消</a></li>
-      <li style="border-right:none"><a href="#" class="sta">使用完成</a></li>
-    </ul>
+        <div style="height:30px;"></div>
+        <div class="row order-title wd">
+            <div class="col-md-8 botsolid"id="title1"><span>购物车</span></div>
+        </div>
+        <div class="row order-head wd">
+            <div class="col-md-8 bot"id="title2">
+                <div class="col-md-2 ">商家会场</div>
+                <div class="col-md-2 ">会场名称</div>
+                <div class="col-md-2 ">所属场地</div>
+                <div class="col-md-2 ">配套服务</div>
+                <div class="col-md-2 ">小计</div>
+                <div class="col-md-2 ">操作</div>
+            </div>
+        </div>
+        <div class="row order-name"></div>
 
-  <table class="style-table " style="margin-top: 50px;">
-      <tr class="style-tr">
+        @if(isset($shopcart))
+            @foreach($shopcart as $k=>$v)
+                <div class="row order-detail wd">
+                    <div class="col-md-8 bot"id="title3">
+                        <div class="col-md-2 ">
+                            <div class="left">
+                                <a href="{{ url('/detail/pid=') }}{{ $v['pid'] }}" target="_blank"><img src="{{ url('uploads/shoper/places/meetplaces/') }}/{{ $v['pic'] }}" width="40" height="40px"></a>
+                            </div>
+                            {{--<div class="left">--}}
+                                {{--<div class="describe"><span>场地方正</span></div>--}}
 
-          <th class="style-td">编号</th>
-          <th class="style-td">商家</th>
-          <th class="style-td">价格</th>
-          <th class="style-td">状态</th>
-          <th class="style-td">详情</th>
-      </tr>
-      @foreach($data as $key=>$val)
-      <tr>
+                            {{--</div>--}}
+                        </div>
+                        <div class="col-md-2 ">{{ $v['mname'] }}</div>
+                        <div class="col-md-2 ">{{ $v['pname'] }}</div>
+                        <div class="col-md-2 ">
+                            @if(isset($v['fname']))
+                                @foreach($v['fname'] as $key=>$val)
+                                    {{ $val }} &nbsp;<br>
+                                @endforeach
+                            @endif
+                        </div>
+                        <div class="col-md-2 shopcartPrice">{{ $v['price'] }}</div>
+                        <div class="col-md-2 "><a href="{{ url('/shopcart/delete') }}/{{ $v['started_at'] }}" class="shopcartDel">删除</a></div>
+                    </div>
+                </div>
+            @endforeach
+        @endif
 
-          <td class="style-td">{{$val->number}}</td>
-          <td class="style-td">{{$val->keepername}}</td>
-          <td class="style-td">{{$val->price}}</td>
-          <td class="style-td">
-              @if($val->status==1)
-                  未付款
-              @elseif($val->status==2)
-                  未接单
-              @elseif($val->status==3)
-                  使用中
-              @elseif($val->status==4)
-                  定单取消
-              @elseif($val->status==5)
-                  使用完成
-              @endif
-          </td>
-          <td class="style-td">1</td>
+        <div class="row wd">
+            <div class="col-md-8 total">
+                <div class="col-md-3 col-md-offset-9">总计: ¥<span class="shopcartTotal"></span></div>
+            </div>
+        </div>
+        <div class="row wd">
+            <div class="col-md-8">
+                <div class="col-md-3 col-md-offset-9"><a class="btn btn-danger" href="{{ url('/home/order/order') }}" role="button" style="color:white;">&nbsp;去结算&nbsp;</a></div>
+            </div>
+        </div>
+        <div style="height:50px;"></div>
 
-      </tr>
-      @endforeach
 
-  </table>
-    {{ $data->appends($request)->links() }}
+    </div>
 
 @endsection
 
 @section('javascript')
     <script>
-
-        //全局变量
-        var id=0;
-
-        $(".del").click(function(){
-
-            id=$(this).parents('.parent').find('.ids').html();
+        var shopcartPrice = $(".shopcartPrice");
+        var shopsum = 0;
+        $.each(shopcartPrice,function(i,n){
+            shopsum += parseInt($(this).html());
         });
-
-
-        // 每页条数下拉框选中事件
-        $('#num').on('change', function(){
-            $('form').eq(0).submit();
-        });
-
-        //根据定单状态的筛选
-        $(".sta").on('click', function(){
-            switch($(this).html())
-            {
-                case '全部订单':
-                    location.href="/usercenter/order/0";
-                    break;
-                case '未付款':
-                    location.href="/usercenter/order/1";
-                    break;
-                case '未接单':
-                    location.href="/usercenter/order/2";
-                    break;
-                case '使用中':
-                    location.href="/usercenter/order/3";
-                    break;
-                case '定单取消':
-                    location.href="/usercenter/order/4";
-                    break;
-                case '使用完成':
-                    location.href="/usercenter/order/5";
-                    break;
-            }
-        });
-
-         $("ul li").click(function(){
-      // alert(111);
-        $(this).addClass("current").siblings().removeClass("current");
-    });
-
+        $(".shopcartTotal").html(shopsum);
     </script>
 @endsection
 
